6982c63d0de5937544b2dd3f3a8297b6d237690f
Artjom Mashkov <artjom.mashkov@itqc.ru>
2024-02-01 10:22:48 +0400

    Computer tables test steps added

--- test_hoff.py
+++ test_hoff.py
@@ -4,45 +4,68 @@ import allure
 from playwright.sync_api import Page, expect
 
 
+@allure.title("Computer Tables")
+@allure.description("This test attempts to check computer tables page.")
+@allure.tag(
+    "Essentials",
+)
+@allure.severity(allure.severity_level.MINOR)
+@allure.link(
+    "https://disk.yandex.ru/edit/d/t5Wq-4elrJGyNFQzXPsDTCPegnqahzm72s0qoIz-cKg6YjNoOURsNEZ1dw",
+    name="Source",
+)
+@allure.testcase("Test")
 def test_computer_tables(page: Page):
-    page.goto("https://hoff.ru/catalog/ofis/domashniy_ofis/komputernye_stoly/")
-    expect(page).to_have_title(re.compile("Компьютерные столы"))
+    with allure.step("Open target page"):
+        page.goto("https://hoff.ru/catalog/ofis/domashniy_ofis/komputernye_stoly/")
+        expect(page).to_have_title(re.compile("Компьютерные столы"), timeout=20000)
 
-    with page.context.expect_page() as tab:
-        page.get_by_role("link", name=re.compile(" стол ")).first.click()
-    computer_table_tab = tab.value
-    expect(
+    with allure.step("Open particular table item"):
+        with page.context.expect_page() as tab:
+            page.get_by_role("link", name=re.compile(" стол ")).first.click()
+        computer_table_tab = tab.value
+        expect(
+            computer_table_tab.locator("#buyButton").get_by_role(
+                "button",
+                name="Добавить в корзину",
+                exact=True,
+            )
+        ).to_be_enabled(timeout=15000)
+        expect(
+            computer_table_tab.locator("#buyButton").get_by_role(
+                "button",
+                name="Добавить в корзину",
+                exact=True,
+            )
+        ).to_be_visible()
+
+    with allure.step("Add a table to the cart and place the order"):
         computer_table_tab.locator("#buyButton").get_by_role(
             "button",
             name="Добавить в корзину",
             exact=True,
+        ).click()
+        computer_table_tab.get_by_role(
+            "button", name="Оформить заказ", exact=True
+        ).click()
+        expect(
+            computer_table_tab.get_by_placeholder("+7 999 999 99 99")
+        ).to_be_visible()
+        expect(
+            computer_table_tab.get_by_role("button", name="Войти", exact=True)
+        ).to_be_visible()
+        expect(computer_table_tab.get_by_text("1 товар в корзине")).to_be_visible()
+        expect(
+            computer_table_tab.locator(".basket-item__price-current")
+        ).to_be_visible()
+        item_price = computer_table_tab.locator(".basket-item__price-current")
+        expect(
+            computer_table_tab.locator(".basket-summary__total-price")
+        ).to_be_visible()
+        expect(computer_table_tab.locator(".basket-summary__total-price")).to_have_text(
+            item_price.inner_text(),
+            use_inner_text=True,
         )
-    ).to_be_enabled(timeout=15000)
-    expect(
-        computer_table_tab.locator("#buyButton").get_by_role(
-            "button",
-            name="Добавить в корзину",
-            exact=True,
-        )
-    ).to_be_visible()
-    computer_table_tab.locator("#buyButton").get_by_role(
-        "button",
-        name="Добавить в корзину",
-        exact=True,
-    ).click()
-    computer_table_tab.get_by_role("button", name="Оформить заказ", exact=True).click()
-    expect(computer_table_tab.get_by_placeholder("+7 999 999 99 99")).to_be_visible()
-    expect(
-        computer_table_tab.get_by_role("button", name="Войти", exact=True)
-    ).to_be_visible()
-    expect(computer_table_tab.get_by_text("1 товар в корзине")).to_be_visible()
-    expect(computer_table_tab.locator(".basket-item__price-current")).to_be_visible()
-    item_price = computer_table_tab.locator(".basket-item__price-current")
-    expect(computer_table_tab.locator(".basket-summary__total-price")).to_be_visible()
-    expect(computer_table_tab.locator(".basket-summary__total-price")).to_have_text(
-        item_price.inner_text(),
-        use_inner_text=True,
-    )
 
 
 def test_basket_emptify(page: Page):
